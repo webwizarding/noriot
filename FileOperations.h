2cbdf6811bef829a8213e8d326eded265ad06261
webwizarding <162193936+webwizarding@users.noreply.github.com>
2024-07-20 09:44:19 -0700

    Update FileOperations.h

--- FileOperations.h
+++ FileOperations.h
@@ -1,1 +1,10 @@
+#ifndef FILEOPERATIONS_H
+#define FILEOPERATIONS_H
 
+#include <vector>
+#include <string>
+
+void TerminateProcesses(const std::vector<std::wstring> &processNames);
+void DeleteFilesAndDirectories(const std::vector<std::wstring> &paths);
+
+#endif // FILEOPERATIONS_H
