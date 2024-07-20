33d2fb71c1cdbb5b6801e7bb09507eb1bb07b2e1
webwizarding <162193936+webwizarding@users.noreply.github.com>
2024-07-20 09:45:23 -0700

    Update ProcessDetection.h

--- ProcessDetection.h
+++ ProcessDetection.h
@@ -1,1 +1,10 @@
+#ifndef PROCESSDETECTION_H
+#define PROCESSDETECTION_H
 
+#include <string>
+#include <vector>
+
+bool IsProcessRunning(const std::wstring &processName);
+bool IsAnyProcessRunning(const std::vector<std::wstring> &processNames);
+
+#endif // PROCESSDETECTION_H
